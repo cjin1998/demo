ace63653f084a2b8f27c5f8ff93e2c9e8e88a13e
cjin1998 <christinejin9@gmail.com>
2021-07-12 11:05:30 -0400

    done

--- myDemo/routes/index.js
+++ myDemo/routes/index.js
@@ -6,12 +6,4 @@ router.get("/", function(req, res, next) {
   res.render("demo", { title: "Demo" });
 });
 
-router.get("/test", function(req, res, next) {
-  res.render("index", { title: "Test" });
-});
-
-router.get("/error", function(req, res, next) {
-  res.render("error", { title: "Error" });
-});
-
 module.exports = router;

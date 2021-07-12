6377bda72319179819e2879b3aa62947779264d0
cjin1998 <christinejin9@gmail.com>
2021-07-11 21:55:41 -0400

    edit validate

--- myDemo/public/javascripts/myScripts.js
+++ myDemo/public/javascripts/myScripts.js
@@ -16,9 +16,11 @@ $(document).ready(function() {
           var firstSlash = value.slice(4, 5);
           var secondSlash = value.slice(7, 8);
 
-          if (isNaN(year) || isNaN(month) || isNaN(day)) {
+          if (isNaN(year) || isNaN(month) || isNaN(day) || value.length != 10) {
             return false;
           } else if (firstSlash != "-" || secondSlash != "-") {
+            return false;
+          } else if (month > 12 || month < 1 || day > 31 || day < 1) {
             return false;
           }
           return true;
@@ -53,7 +55,7 @@ $(document).ready(function() {
           inputEmail: "Please enter a valid email address",
           inputDate: {
             required: "Please enter your birthday",
-            dateFormat: "Date format must be yyyy-mm-dd"
+            dateFormat: "Please enter a valid date (yyyy-mm-dd)"
           }
         }
       });
@@ -75,13 +77,13 @@ $(document).ready(function() {
       var birthday = $("#inputDate").val();
 
       $(".tbody").append(
-        "<tr><td class='name'>" +
+        "<tr><td class='name'><form name='changeName' class='changeName'>" +
           name +
-          "</td><td class='email'>" +
+          "</form></td><td class='email'><form name='changeEmail' class='changeEmail'>" +
           email +
-          "</td><td class='birthday'>" +
+          "</form></td><td class='birthday'><form name='changeDate' class='changeDate'>" +
           birthday +
-          '</td><td><button type="button" class="edit">Edit</button></td></tr>'
+          '</form></td><td><button type="button" class="edit">Edit</button></td></tr>'
       );
 
       $("#inputName").val("");
@@ -94,110 +96,203 @@ $(document).ready(function() {
 
   $(document).on("click", ".edit", function() {
     if ($(this).text() == "Edit") {
+      jQuery.validator.addMethod(
+        "dateFormat",
+        function(value, element) {
+          var year = parseInt(value.slice(0, 4));
+          var month = parseInt(value.slice(5, 7));
+          var day = parseInt(value.slice(8));
+
+          var firstSlash = value.slice(4, 5);
+          var secondSlash = value.slice(7, 8);
+
+          if (isNaN(year) || isNaN(month) || isNaN(day) || value.length != 10) {
+            return false;
+          } else if (firstSlash != "-" || secondSlash != "-") {
+            return false;
+          } else if (month > 12 || month < 1 || day > 31 || day < 1) {
+            return false;
+          }
+          return true;
+        },
+        ""
+      );
+
+      $(this)
+        .closest("tr")
+        .find("form[name='changeName']")
+        .validate({
+          rules: {
+            editName: {
+              required: true,
+              minlength: 2
+            }
+          },
+
+          messages: {
+            editName: {
+              required: "Please enter your name",
+              minlength: "Name must be at least 2 characters long"
+            }
+          }
+        });
+
+      $(this)
+        .closest("tr")
+        .find("form[name='changeEmail']")
+        .validate({
+          rules: {
+            editEmail: {
+              required: true,
+              email: true
+            }
+          },
+
+          messages: {
+            editEmail: "Please enter a valid email address"
+          }
+        });
+
+      $(this)
+        .closest("tr")
+        .find("form[name='changeDate']")
+        .validate({
+          rules: {
+            editBirthday: {
+              required: true,
+              dateFormat: true
+            }
+          },
+
+          messages: {
+            editBirthday: {
+              required: "Please enter your birthday",
+              dateFormat: "Please enter a valid date (yyyy-mm-dd)"
+            }
+          }
+        });
+
       $(this).html("Save");
 
       var name = $(this)
         .closest("tr")
-        .find(".name")
+        .find(".changeName")
         .text();
 
       $(this)
         .closest("tr")
-        .find(".name")
+        .find(".changeName")
         .html("");
 
       $(this)
         .closest("tr")
-        .find(".name")
+        .find(".changeName")
         .append(
           '<input type="text" value="' +
             name +
-            '" class="form-control editName">'
+            '" class="form-control editName" name="editName">'
         );
 
       var email = $(this)
         .closest("tr")
-        .find(".email")
+        .find(".changeEmail")
         .text();
 
       $(this)
         .closest("tr")
-        .find(".email")
+        .find(".changeEmail")
         .html("");
 
       $(this)
         .closest("tr")
-        .find(".email")
+        .find(".changeEmail")
         .append(
           '<input type="text" value="' +
             email +
-            '" class="form-control editEmail">'
+            '" class="form-control editEmail" name="editEmail">'
         );
 
       var birthday = $(this)
         .closest("tr")
-        .find(".birthday")
+        .find(".changeDate")
         .text();
 
       $(this)
         .closest("tr")
-        .find(".birthday")
+        .find(".changeDate")
         .html("");
 
       $(this)
         .closest("tr")
-        .find(".birthday")
+        .find(".changeDate")
         .append(
           '<input type="text" value="' +
             birthday +
-            '" class="form-control editBirthday">'
+            '" class="form-control editBirthday" name="editBirthday">'
         );
 
       $(this)
         .closest("td")
         .append('<button type="button" class="ms-1 delete">Delete</button>');
     } else {
-      $(this).html("Edit");
-      //save button functions
-      var newName = $(this)
-        .closest("tr")
-        .find(".editName")
-        .val();
+      if (
+        $(this)
+          .closest("tr")
+          .find("form[name='changeName']")
+          .valid() &&
+        $(this)
+          .closest("tr")
+          .find("form[name='changeEmail']")
+          .valid() &&
+        $(this)
+          .closest("tr")
+          .find("form[name='changeDate']")
+          .valid()
+      ) {
+        $(this).html("Edit");
+        //save button functions
+        var newName = $(this)
+          .closest("tr")
+          .find(".editName")
+          .val();
 
-      var newEmail = $(this)
-        .closest("tr")
-        .find(".editEmail")
-        .val();
+        var newEmail = $(this)
+          .closest("tr")
+          .find(".editEmail")
+          .val();
 
-      var newBirthday = $(this)
-        .closest("tr")
-        .find(".editBirthday")
-        .val();
+        var newBirthday = $(this)
+          .closest("tr")
+          .find(".editBirthday")
+          .val();
 
-      $(this)
-        .closest("tr")
-        .find("input")
-        .remove();
+        $(this)
+          .closest("tr")
+          .find("input")
+          .remove();
 
-      $(this)
-        .closest("tr")
-        .find(".name")
-        .html(newName);
+        $(this)
+          .closest("tr")
+          .find(".changeName")
+          .html(newName);
 
-      $(this)
-        .closest("tr")
-        .find(".email")
-        .html(newEmail);
+        $(this)
+          .closest("tr")
+          .find(".changeEmail")
+          .html(newEmail);
 
-      $(this)
-        .closest("tr")
-        .find(".birthday")
-        .html(newBirthday);
+        $(this)
+          .closest("tr")
+          .find(".changeDate")
+          .html(newBirthday);
 
-      $(this)
-        .closest("td")
-        .find(".delete")
-        .hide();
+        $(this)
+          .closest("td")
+          .find(".delete")
+          .remove();
+      } else {
+        alert("Please complete the form correctly before submission");
+      }
     }
   });
 
@@ -216,16 +311,4 @@ $(document).ready(function() {
       $(this).css("color", "white");
     }
   );
-
-  $("#but").click(function() {
-    $(this).val(function(n, c) {
-      var number = parseInt(c) + 1;
-      alert("you have clicked " + number + " times!");
-      return number;
-    });
-  });
 });
-
-function myFunction() {
-  document.getElementById("demo").innerHTML = "Paragraph changed.";
-}
